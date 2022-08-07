d77553bb488002143850a30a03fb0f9fed7b6c74
Nad <34311347+nadnone@users.noreply.github.com>
2022-08-07 03:02:23 +0200

    Update gallery.js

--- config/gallery.js
+++ config/gallery.js
@@ -3,7 +3,7 @@ export default [
         "title": "Raycaster (2022)",
         "desc": "Just for learn, because I wanted to learn it",
         "link": "https://github.com/nadnone/ray_caster",
-        "image": "https://github.com/nadnone/ray_caster/blob/master/res/demo.png?raw=true"
+        "image": "https://github.com/nadnone/ray_caster/blob/master/res/demo.gif?raw=true"
     },  
     {
         "title": "Rust rasterizer (2022)",
